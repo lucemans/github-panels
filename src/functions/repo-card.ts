d9a20226228039f4953ccc541579e93261d97b5c
Lucemans <luc@lucemans.nl>
2021-10-30 22:45:29 +0200

    Package Fetching

--- src/functions/repo-card.ts
+++ src/functions/repo-card.ts
@@ -1,19 +1,81 @@
 import { APIGatewayEvent, Context } from 'aws-lambda';
-import SvgBuilder from 'svg-builder';
-import { createCard } from '../lib/svg';
+import { createCard, createSVG, SvgBuilder } from '../lib/svg';
+import axios from 'axios';
 
-export async function handler (
-  event: APIGatewayEvent,
-  context: Context
+export async function handler(
+    event: APIGatewayEvent,
+    context: Context
 ) {
 
-  const svg = createCard();
+    const pkg = event.queryStringParameters['package'];
 
-  return {
-    statusCode: 200,
-     headers: {
-      'Content-Type': 'image/svg+xml'
-    },
-    body: svg
-  }
+    const pkgdata: {
+        '_id': string,
+        '_rev': string,
+        'name': string,
+        'dist-tags': { [key: string]: string },
+        'versions': { [key: string]: { name: string, version: string, description: string, main: string, types: string } },
+        'time': { [key: string]: string },
+        'maintainers': { name: string, email: string }[],
+        'description': string,
+        'keywords': string[],
+        'repository': string | { type: string, url: string },
+        'author': string | { name: string, email: string },
+        'license': string,
+        'readme': string,
+        'readmeFilename': string
+    } = (await axios.get('https://registry.npmjs.org/' + pkg)).data;
+    const pkgdownload: {
+        "downloads": number,
+        "start": string,
+        "end": string,
+        "package": string
+    } = (await axios.get('https://api.npmjs.org/downloads/point/last-year/' + pkg)).data;
+
+    const svg = SvgBuilder.newInstance();
+
+    svg.root += `<style>
+    .name {
+        font: bold 30px sans-serif;
+        fill: white;
+    }
+    .downloads {
+        font: bold 15px sans-serif;
+        fill: white;
+    }
+    </style>`;
+
+    svg.width(480);
+    svg.height(120);
+
+    svg.rect({
+        x: 0.5,
+        y: 0.5,
+        rx: 4.5,
+        height: "99%",
+        stroke: "#e4e2e2",
+        width: 479,
+        fill: "#2e3440",
+        "stroke-opacity": 0
+    });
+
+    svg.text({
+        x: 100,
+        y: 50,
+        class: "name",
+    }, pkgdata.name);
+
+    svg.text({
+        x: 100,
+        y: 75,
+        class: "downloads",
+    }, pkgdownload.downloads + " downloads");
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'image/svg+xml'
+        },
+        body: svg.render()
+    }
 }
--- src/lib/svg.ts
+++ src/lib/svg.ts
@@ -1,7 +1,27 @@
+
+import SvgBuilder2 from 'svg-builder';
+
+type ElemData = {[key: string]: string | number};
+
+type SvgBuilderType = {
+    root: string,
+    render: () => string,
+    rect: (a: ElemData) => void,
+    width: (w: number) => void,
+    height: (h: number) => void,
+    text: (a: ElemData, s: string) => void,
+    addElement: (a: {[key: string]: unknown}) => void,
+    newInstance: () => SvgBuilderType
+}
+
+export const SvgBuilder = SvgBuilder2 as SvgBuilderType;
+
 export const createCard = () => {
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="480" height="120" viewBox="0 0 480 120" fill="none">
     <rect xmlns="http://www.w3.org/2000/svg" data-testid="card-bg" x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="479" fill="#2e3440" stroke-opacity="0"/>
-    </svg>
     `;
+};
+
+export const createSVG = (body) => {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="480" height="120" viewBox="0 0 480 120" fill="none">${body}`;
 };
