24bd0d2c445db8cfac8c8969581738d06199cacf
Lucemans <luc@lucemans.nl>
2021-10-30 20:52:54 +0200

    Lowered Card Width

--- src/lib/svg.ts
+++ src/lib/svg.ts
@@ -1,6 +1,6 @@
 export const createCard = () => {
     return `
-    <svg xmlns="http://www.w3.org/2000/svg" width="495" height="120" viewBox="0 0 495 120" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="480" height="120" viewBox="0 0 495 120" fill="none">
     <rect xmlns="http://www.w3.org/2000/svg" data-testid="card-bg" x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="494" fill="#2e3440" stroke-opacity="0"/>
     </svg>
     `;
